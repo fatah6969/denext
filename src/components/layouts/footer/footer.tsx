002053622c3c4a8bb07e524e34ad9c5f66df095b
Abdul Fatah <info.denext@gmail.com>
2025-08-20 11:53:54 +0700

    Update footer.tsx

--- src/components/layouts/footer/footer.tsx
+++ src/components/layouts/footer/footer.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import { Text, Group, ActionIcon, Box } from "@mantine/core";
-import {
-  IconBrandFacebook,
-  IconBrandInstagram,
-  IconBrandLinkedin,
-  IconBrandGithub,
-} from "@tabler/icons-react";
-
+import { Text, Flex, Box } from "@mantine/core";
 export function Footer() {
   return (
       <Box
@@ -19,57 +12,11 @@ export function Footer() {
         borderTop: "1px solid #e9ecef",
       }}
     >
-      <Group justify="space-between" align="center">
+      <Flex justify="center" align="center">
         <Text size="sm" c="dimmed">
           © {new Date().getFullYear()} DENEXT. All rights reserved.
-        </Text>
-
-        <Group gap="xs">
-          <ActionIcon
-            component="a"
-            href="https://facebook.com"
-            target="_blank"
-            variant="subtle"
-            size="lg"
-            aria-label="Facebook"
-          >
-            <IconBrandFacebook size={20} />
-          </ActionIcon>
-
-          <ActionIcon
-            component="a"
-            href="https://instagram.com"
-            target="_blank"
-            variant="subtle"
-            size="lg"
-            aria-label="Instagram"
-          >
-            <IconBrandInstagram size={20} />
-          </ActionIcon>
-
-          <ActionIcon
-            component="a"
-            href="https://linkedin.com"
-            target="_blank"
-            variant="subtle"
-            size="lg"
-            aria-label="LinkedIn"
-          >
-            <IconBrandLinkedin size={20} />
-          </ActionIcon>
-
-          <ActionIcon
-            component="a"
-            href="https://github.com"
-            target="_blank"
-            variant="subtle"
-            size="lg"
-            aria-label="GitHub"
-          >
-            <IconBrandGithub size={20} />
-          </ActionIcon>
-        </Group>
-      </Group>
+        </Text>  
+      </Flex>
     </Box>
   );
 }
