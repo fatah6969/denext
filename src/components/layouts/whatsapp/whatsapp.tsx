14c0e74a5786ef682a499ddb404031d773dd1839
Abdul Fatah <info.denext@gmail.com>
2025-08-27 14:02:16 +0700

    Update whatsapp.tsx

--- src/components/layouts/whatsapp/whatsapp.tsx
+++ src/components/layouts/whatsapp/whatsapp.tsx
@@ -1,25 +1,38 @@
-import { ActionIcon } from "@mantine/core";
+import { ActionIcon, useMantineTheme } from "@mantine/core";
 import { IconBrandWhatsapp } from "@tabler/icons-react";
 
 export function WhatsappFloating() {
+  const theme = useMantineTheme();
+
   return (
-            <ActionIcon
-              variant="filled"
-              aria-label="Whatsapp"
-              size="3rem"
-              color="green"
-              radius="100%"
-              style={{ position: "fixed", bottom: 20, right: 20 }}
-              component="a"
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={() => window.open("https://wa.me/6285117477481")}
-            >
-              <IconBrandWhatsapp
-                style={{ width: "70%", height: "70%" }}
-                size={20}
-                stroke={1.5}
-              />
-            </ActionIcon>
-  )
+    <ActionIcon
+      component="a"
+      href="https://wa.me/6285117477481"
+      target="_blank"
+      rel="noopener noreferrer"
+      variant="filled"
+      color="green"
+      radius="100%"
+      size={48} // default size desktop
+      style={{
+        position: "fixed",
+        bottom: 20,
+        right: 20,
+      }}
+      sx={{
+        [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+          size: 40, // lebih kecil di HP
+        },
+      }}
+    >
+      <IconBrandWhatsapp
+        size={32} // desktop
+        stroke={1.5}
+        style={{
+          width: "70%",
+          height: "70%",
+        }}
+      />
+    </ActionIcon>
+  );
 }
