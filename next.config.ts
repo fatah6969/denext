9d22d05dd6b30afa788a4885fb07faf1bb10f331
Abdul Fatah <info.denext@gmail.com>
2026-02-26 11:08:48 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.siliwangiexpress.web.id",
-      },
-    ],
-  },
+  /* config options here */ 
 };
 
 export default nextConfig;
